e59e59a6c29c22505ef7bd2bc581b81abbddff72
Praveen S <51072726+i-ampraveen@users.noreply.github.com>
2024-09-27 22:31:48 +0530

    Created login scenario and step definition file boilerplate

--- src/test/java/stepDefinitions/Login.java
+++ src/test/java/stepDefinitions/Login.java
@@ -1,37 +1,44 @@
+package stepDefinitions;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
 public class Login {
-    @io.cucumber.java.en.Given("^User enters \"([^\"]*)\" in the \"([^\"]*)\"$")
+    @Given("^User enters \"([^\"]*)\" in the \"([^\"]*)\"$")
     public void userEntersInThe(String arg0, String arg1) throws Throwable {
         // Write code here that turns the phrase above into concrete actions
         //throw new cucumber.api.PendingException();
     }
 
-    @io.cucumber.java.en.Then("^User is in \"([^\"]*)\" homepage$")
+    @Then("^User is in \"([^\"]*)\" homepage$")
     public void userIsInHomepage(String arg0) throws Throwable {
         // Write code here that turns the phrase above into concrete actions
         //throw new cucumber.api.PendingException();
     }
 
-    @io.cucumber.java.en.When("^Enters \"([^\"]*)\"$")
+    @When("^Enters \"([^\"]*)\"$")
     public void enters(String arg0) throws Throwable {
         // Write code here that turns the phrase above into concrete actions
         //throw new cucumber.api.PendingException();
     }
 
-    @io.cucumber.java.en.When("^Login$")
+    @When("^stepDefinitions.Login$")
     public void login() {
     }
 
-    @io.cucumber.java.en.Then("^User logged in successfully$")
+    @Then("^User logged in successfully$")
     public void userLoggedInSuccessfully() {
     }
 
-    @io.cucumber.java.en.Then("^\"([^\"]*)\" message is displayed$")
+    @Then("^\"([^\"]*)\" message is displayed$")
     public void messageIsDisplayed(String arg0) throws Throwable {
         // Write code here that turns the phrase above into concrete actions
         //throw new cucumber.api.PendingException();
     }
 
-    @io.cucumber.java.en.And("^login is unsuccessful$")
+    @And("^login is unsuccessful$")
     public void loginIsUnsuccessful() {
     }
 }
---
+++ src/test/resources/functionalTests/Login.feature
@@ -0,0 +1,40 @@
+Feature: Login
+  Description: Testing login scenario in swag labs
+
+  Rule: User login in swag labs
+    Background:
+      Given User enters "URL" in the "browser"
+      Then User is in "swag labs" homepage
+
+
+Scenario: Successful login as standard user
+  When Enters "standard_user"
+  * Enters "secret_sauce"
+  * Login
+  Then User logged in successfully
+
+Scenario: Login as locked-out-user
+  When Enters "locked_out_user"
+  * Enters "secret_sauce"
+  Then "User is locked out" message is displayed
+  And login is unsuccessful
+
+Scenario: Login as problem-user
+  When Enters "problem_user"
+  * Enters "secret_sauce"
+  Then User logged in successfully
+
+Scenario: Login as performance-glitch-user
+  When Enters "performance_glitch_user"
+  * Enters "secret_sauce"
+  Then User logged in successfully
+
+Scenario: Login as error-user
+  When Enters "error_user"
+  * Enters "secret_sauce"
+  Then User logged in successfully
+
+Scenario: Login as visual-user
+  When Enters "visual_user"
+  * Enters "secret_sauce"
+  Then User logged in successfully
