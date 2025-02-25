c279d29ac01473ab7b9bd25b18902fadc1dcb16c
Praveen S <51072726+i-ampraveen@users.noreply.github.com>
2025-02-26 00:35:53 +0530

    Added README.md file.

--- src/test/java/runners/TestRunner.java
+++ src/test/java/runners/TestRunner.java
@@ -7,7 +7,7 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         features = "src/test/resources/functionalTests",
-        glue = { "StepDefinitions" },
+        glue = { "stepDefinitions" },
         plugin = { "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:" },
         monochrome = true
 )
